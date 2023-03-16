08f8447f9db151cd3f474994bd32b14500b6e75e
Minoka Wickramasinghe <induwaraminoka@gmail.com>
2023-03-16 07:32:27 +0530

    new input system

--- Assets/Scripts/GameInputs.cs
+++ Assets/Scripts/GameInputs.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class GameInputs : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+    private void Awake()
+    {
+        playerInputActions =  new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
     public Vector2 GetMovementVectorNormalized()
     {
         //Inputs
-        Vector2 inputVector = new Vector2(0, 0);
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
+        /*
+         * old input method
+         * 
+         * 
         if (Input.GetKey(KeyCode.W))
         {
             inputVector.y = +1;
@@ -25,6 +35,9 @@ public class GameInputs : MonoBehaviour
         {
             inputVector.x = +1;
         }
+
+
+        */
 
         inputVector = inputVector.normalized;
         return inputVector;
