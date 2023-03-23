ea4770b2d68026c076969699ca580b83c835fefe
Minoka Wickramasinghe <induwaraminoka@gmail.com>
2023-03-23 14:21:14 +0530

    Kitchen Object Scriptable

--- Assets/Scripts/KitchenObjectsO.cs
+++ Assets/Scripts/KitchenObjectsO.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu()]
 public class KitchenObjectsO : ScriptableObject
 {
     public Transform prefab;
+    public Sprite sprite;
+    public string objectfName;
 }
