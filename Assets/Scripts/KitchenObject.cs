0cdb70e67614d66fb5ae29fe08d5507f7982ee8a
Minoka Wickramasinghe <induwaraminoka@gmail.com>
2023-03-23 14:45:35 +0530

    Kitchenob

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class KitchenObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private KitchenObjectSO kitchenObjectSO;
 
-    // Update is called once per frame
-    void Update()
+    public KitchenObjectSO GetKitchenObjectSO()
     {
-        
+        return kitchenObjectSO;
     }
 }
